17b4c2953d3e56b13de0bbf609bdd80a2de1adb7
hooleeguns51 <mrsuh3343@gmail.com>
2025-05-20 13:31:26 +0900

    fix: updated divide function

--- functions.py
+++ functions.py
@@ -8,3 +8,8 @@ def minus(a, b):
 
 def divide(a, b):
 		pass
+def divide(a, b):
+    """Returns the result of dividing a by b."""
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
